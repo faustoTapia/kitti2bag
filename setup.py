5aa4fe4869d7de2b2c404cbd8482042f57e46397
Fausto Tapia <ftstalin_csg@hotmail.com>
2021-03-25 10:01:51 +0100

    Changing setup file

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     description='Convert KITTI dataset to ROS bag file the easy way!',
     author='Tomas Krejci',
     author_email='tomas@krej.ci',
-    url='https://github.com/tomas789/kitti2bag/',
-    download_url = 'https://github.com/tomas789/kitti2bag/archive/1.5.zip',
+    url='https://github.com/faustoTapia/kitti2bag.git@bug_fix_odom_p',
+    download_url = 'https://github.com/faustoTapia/kitti2bag/archive/1.5.zip',
     keywords = ['dataset', 'ros', 'rosbag', 'kitti'],
     entry_points = {
         'console_scripts': ['kitti2bag=kitti2bag.__main__:main'],
